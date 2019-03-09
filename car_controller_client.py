0bdff0bcf1f2bdfd60269bb88bd90969218ef39f
DESKTOP-K8SFOJ5\bellm <bellmr@gmail.com>
2019-03-09 16:20:10 -0500

    Got the car client working just fine.

--- car_controller_client.py
+++ car_controller_client.py
@@ -3,7 +3,7 @@ A simple script to connect a remote car and send controller inputs.
 """
 # pylint: disable=E1101
 import numpy as np
-import sys, time
+import sys, time, os
 import pygame
 import socket
 import struct
@@ -16,20 +16,25 @@ host = sys.argv[1]
 port = int(sys.argv[2])
 server_addr = (host, port)
 
-pygame.init()  
+pygame.init() 
 
 # Set parameters and constants
+size = width, height = 200, 100
 FPS = 10
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+DEADZONE = 0.15
 
 if pygame.joystick.get_count() > 0:
-    pygame.joystick.init()
     joystick = pygame.joystick.Joystick(0)
     joystick.init()
+    pygame.joystick.init()
     use_joystick = True
 else:
     use_joystick = False
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("RPiCar Client")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(b'hiya', server_addr)
@@ -39,6 +44,13 @@ print('Starting control loop')
 
 while True:
     t0 = time.time()
+
+    # Event loop    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  
+            sys.exit()
+
+    screen.fill(WHITE)
 
     if use_joystick:
         left_right = joystick.get_axis(0)
@@ -52,6 +64,9 @@ while True:
             pygame.key.get_pressed()[pygame.K_DOWN] * -1.0 + 
             pygame.key.get_pressed()[pygame.K_UP] * 1.0
         )
+    
+    if abs(left_right) < DEADZONE:
+        left_right = 0.0
 
     print('\r', end='')
     print(
@@ -62,6 +77,8 @@ while True:
     joy_input = struct.pack('>f', left_right) + struct.pack('>f', front_back)
 
     sock.sendto(joy_input, server_addr)
+
+    pygame.display.flip()
 
     # Cap the FPS
     elapsed_time = time.time() - t0
